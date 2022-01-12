cf5c3ea58bd0ab1bbc5b0bfc53d03c7ef9c413ac
OFUZOR CHUKWUEMEKE <uniben2018@gmail.com>
2022-01-12 22:32:47 +0100

    new product

--- router/product.js
+++ router/product.js
@@ -1,5 +1,8 @@
 const router = require("express").Router()
 const Product = require("../models/Product")
+const { verifyTokenAndAdmin } = require("../verifyToken")
+
+
 router.post("/",verifyTokenAndAdmin,async(req,res)=>{
     const newProduct = new Product(req.body)
 
@@ -11,5 +14,27 @@ router.post("/",verifyTokenAndAdmin,async(req,res)=>{
     }
 })
 
+
+router.put("/:id",verifyTokenAndAdmin,async(req,res)=>{
+    
+    try{
+        
+        const updatedProduct = await Product.findByIdAndUpdate(req.params.id,{
+            $set:req.body
+        },{new:true})
+        res.status(200).json(updatedProduct)
+    }catch(err){
+        res.status(500).json(err)
+    } 
+})
+
+router.delete("/:id",verifyTokenAndAdmin,async(req,res)=>{
+    try{ 
+        await Product.findByIdAndDelete(req.params.id)
+        res.status(200).json("Product has been Deleted")
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router
